31c2a00ca37eacfae0d3daa12f1011fba5837569
Nathrac <nathanial.zaina@lethbridgecollege.ca>
2021-09-23 10:09:50 -0600

    InitialCommit

---
+++ Sandbox/Assets/Scripts/objectScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectScript : MonoBehaviour
+{
+
+    Renderer objRenderer;
+    float timer = 0f;
+    [SerializeField]
+    private float colourChangeTime;
+    float yPos;
+    [SerializeField]
+    [Range(0, 5)]
+    float yRange;
+    [SerializeField]
+    float floatSpeed;
+    bool isCorrect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objRenderer = GetComponent<Renderer>();
+        timer += Time.deltaTime;
+
+        if (timer >= colourChangeTime)
+        {
+            Debug.Log(timer);
+            Color newColor = Random.ColorHSV();
+            objRenderer.material.color = newColor;
+            timer = 0;
+        }
+        yPos = Mathf.PingPong(Time.time * floatSpeed, 1) * yRange;
+        transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+
+    }
+    private void OnMouseDown()
+    {
+        if (isCorrect)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
